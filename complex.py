34fbfbf201871238706564a5a504ea60c292c0a0
Felipe Aguas <juan.aguas@escuelaing.edu.co>
2020-08-07 19:40:24 -0500

    Finished exercises

--- complex.py
+++ complex.py
@@ -1,8 +1,10 @@
 # Autor: Juan Felipe Aguas Pulido
 import math
 
+
 def add(c1, c2):
     """
+    Adds two complex numbers
     :list c1: List of two items, first one the real part and second one the imaginary part
     :list c2: List of two items, first one the real part and second one the imaginary part
     :return list:
@@ -13,6 +15,7 @@ def add(c1, c2):
 
 def subs(c1, c2):
     """
+    Substracts two complex numbers
     :list c1: List of two items, first one the real part and second one the imaginary part
     :list c2: List of two items, first one the real part and second one the imaginary part
     :return list:
@@ -23,6 +26,7 @@ def subs(c1, c2):
 
 def mult(c1, c2):
     """
+    Multiplies two complex numbers
     :list c1: List of two items, first one the real part and second one the imaginary part
     :list c2: List of two items, first one the real part and second one the imaginary part
     :return list:
@@ -33,26 +37,29 @@ def mult(c1, c2):
 
 def divi(c1, c2):
     """
+    Divides two complex numbers
     :list c1: List of two items, first one the real part and second one the imaginary part
     :list c2: List of two items, first one the real part and second one the imaginary part
     :return list:
     """
     den = c2[0] ** 2 + c2[1] ** 2
-    ans = [(c1[0] * c2[0] + c1[1] * c2[1]) / den, (c2[0] * c1[1] - c1[0] * c2[1]) / den]
+    ans = [round((c1[0] * c2[0] + c1[1] * c2[1]) / den, 3), round((c2[0] * c1[1] - c1[0] * c2[1]) / den, 3)]
     return ans
 
 
 def mod(c):
     """
+    Returns the module of a complex number
     :list c: List of two items, first one the real part and second one the imaginary part
-    :return list:
+    :return float:
     """
-    ans = (c[0] ** 2 + c[1] ** 2)** 0.5
-    return ans
+    ans = (c[0] ** 2 + c[1] ** 2) ** 0.5
+    return round(ans, 3)
 
 
 def conj(c):
     """
+    Returns the conjugated of a complex number
     :list c: List of two items, first one the real part and second one the imaginary part
     :return list:
     """
@@ -60,21 +67,23 @@ def conj(c):
     return ans
 
 
-def coorToPolar(c):
+def coor_to_polar(c):
     """
+    Changes a complex number from coordinates to Polar
     :list c: List of two items, first one the real part and second one the imaginary part
     :return list:
     """
     p = mod(c)
     theta = math.atan(c[1] / c[0])
-    ans = [p, theta]
+    ans = [round(p, 3), round(theta, 3)]
     return ans
 
 
 def phase(c):
     """
+    Returns the phase of a complex number
     :list c: List of two items, first one the real part and second one the imaginary part
     :return list:
     """
     ans = math.atan(c[1] / c[0])
-    return ans
+    return round(ans, 3)
